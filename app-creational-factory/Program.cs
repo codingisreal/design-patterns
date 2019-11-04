76b79adf44dd931794955ed1e069f6a61a648f18
gwairagade <gwairagade@silica.net>
2019-11-04 12:46:38 +0530

    New practice projects for Factory Pattern

---
+++ app-creational-factory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_creational_factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
