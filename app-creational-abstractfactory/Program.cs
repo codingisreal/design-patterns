5b575b0e55c2c9399a311ef8427b25d70f85b2ab
gwairagade <gwairagade@silica.net>
2019-11-05 15:40:56 +0530

    Design patterns implemented

---
+++ app-creational-abstractfactory/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_creational_abstractfactory
+{
+    public interface IMotorbike
+    {
+        string Info();
+    }
+
+    public class Adv : IMotorbike
+    {
+        public string Info()
+        {
+            return "Ready for Adventure !";
+        }
+    }
+
+    public class SportsBike : IMotorbike
+    {
+        public string Info()
+        {
+            return "Ready to Race !";
+        }
+    }
+
+    interface MotorbikeFactory
+    {
+        IMotorbike GetMotorbike();
+    }
+
+    public class AdvFactory
+    {
+        public IMotorbike GetMotorbike()
+        {
+            return new Adv();
+        }
+    }
+
+    public class SportsBikeFactory
+    {
+        public IMotorbike GetMotorbike()
+        {
+            return new SportsBike();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMotorbike motorbike;
+
+            AdvFactory _advFactory = new AdvFactory();
+            motorbike = _advFactory.GetMotorbike();
+            Console.WriteLine(motorbike.Info());            
+
+            SportsBikeFactory _sportsBikeFactory = new SportsBikeFactory();
+            motorbike = _sportsBikeFactory.GetMotorbike();
+            Console.WriteLine(motorbike.Info());
+            Console.Read();
+        }
+    }
+}
--- app-creational-factory/Program.cs
+++ app-creational-factory/Program.cs
@@ -2,11 +2,83 @@
 
 namespace app_creational_factory
 {
+    public interface ICarMaker
+    {
+        string Info();
+    }
+    public class CarMaker
+    {
+        public string name;
+        public string origin;
+    }
+
+    public class Porsche : CarMaker, ICarMaker
+    {
+        public Porsche()
+        {
+            this.name = "Porsche";
+            this.origin = "Germany";
+        }
+        public string Info()
+        {
+            return String.Format("I am {0} and my origin is {1}", this.name, this.origin);
+        }
+    }
+
+    public class Mercedes : CarMaker, ICarMaker
+    {
+        public Mercedes()
+        {
+            this.name = "Mercedes";
+            this.origin = "Germany";
+        }
+
+        public string Info()
+        {
+            return String.Format("I am {0} and my origin is {1}", this.name, this.origin);
+        }
+    }
+
+    public abstract class CarFactory
+    {
+        public abstract ICarMaker GetMaker();
+        public string CarMakerInfo(CarMaker carMaker)
+        {
+            return String.Format("The car maker comes from {0}", carMaker.origin);
+        }
+    }
+
+    public class PorscheFactory : CarFactory
+    {
+        public override ICarMaker GetMaker()
+        {
+            return new Porsche();
+        }
+    }
+
+    public class MercedesFactory : CarFactory
+    {
+        public override ICarMaker GetMaker()
+        {
+            return new Mercedes();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            var porscheFactory = new PorscheFactory();
+            var mercedesFactory = new MercedesFactory();
+            ICarMaker porsche = porscheFactory.GetMaker();
+            ICarMaker mercedes = mercedesFactory.GetMaker();
+
+            Console.WriteLine(porsche.Info());
+            Console.WriteLine(mercedes.Info());
+            //Console.WriteLine(porsche.C());
+            //Console.WriteLine(porscheFactory.CarMakerInfo(porsche));
+            Console.Read();
         }
     }
 }
